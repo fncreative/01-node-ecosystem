930f5fd1b6127dece399872ea340c0740dfb1082
Daniel Frey <dann.frey@me.com>
2018-09-11 17:12:20 -0700

    added files

--- src/lib/arithmetic.js
+++ src/lib/arithmetic.js
@@ -3,25 +3,25 @@
 const arithmetic = module.exports = {};
 
 arithmetic.add = (num1, num2) => {
-    try {
-        if (num1 === '' || typeof num1 !== 'number' || num2 === '' || typeof num2 !== 'number') {
-            throw new Error('Inputs must be numbers');
-        }
-        return num1 + num2;
-    } catch (err) {
-        console.error(err);
+  try {
+    if (num1 === '' || typeof num1 !== 'number' || num2 === '' || typeof num2 !== 'number') {
+      throw new Error('Inputs must be numbers');
     }
-    return null;
+    return num1 + num2;
+  } catch (err) {
+    console.error(err);
+  }
+  return null;
 };
 
 arithmetic.sub = (num1, num2) => {
-    try {
-        if (num1 === '' || typeof num1 !== 'number' || num2 === '' || typeof num2 !== 'number') {
-            throw new Error('Inputs must be numbers');
-        }
-        return num1 - num2;
-    } catch (err) {
-        console.error(err);
+  try {
+    if (num1 === '' || typeof num1 !== 'number' || num2 === '' || typeof num2 !== 'number') {
+      throw new Error('Inputs must be numbers');
     }
-    return null;
+    return num1 - num2;
+  } catch (err) {
+    console.error(err);
+  }
+  return null;
 };
